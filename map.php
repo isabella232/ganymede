c7f49d9a741a016873faeb1c3a772130419a61d6
ngervais <ngervais>
2008-05-15 15:43:03 +0000

    *** empty log message ***

--- map.php
+++ map.php
@@ -41,8 +41,8 @@
   	  		</td>
   	  		<td width="300" valign="top">
   	  			<div id="filters">
-  	  				<input type="checkbox" name="text" checked/> Text<br/>
-  	  				<input type="checkbox" name="media" checked/> Media<br/>
+  	  				<input type="checkbox" name="text" checked onclick="toggleType('text')"/> Text<br/>
+  	  				<input type="checkbox" name="media" checked onclick="toggleType('media')"/> Media<br/>
   	  			</div>
   	  		</td>
   	  	</tr>
@@ -62,23 +62,39 @@
       // arrays to hold copies of the markers used by the side_bar
       // because the function closure trick doesnt work there
       var gmarkers = [];
+      var gmarkersType = [];
       var i = 0;
 
 
       // A function to create the marker and set up the event window
-      function createMarker(point,name,html) {
+      function createMarker(point,name,html,type) {
         var marker = new GMarker(point);
         GEvent.addListener(marker, "click", function() {
           marker.openInfoWindowHtml(html);
         });
         // save the info we need to use later for the side_bar
         gmarkers[i] = marker;
+        gmarkersType[i] = type;
         // add a line to the side_bar html
         list_html += '<a href="javascript:myclick(' + i + ')">' + name + '</a><br>';
         i++;
         return marker;
       }
-
+	
+	  function toggleType(type)
+	  {
+	  	for (i=0; i<gmarkers.length; i++)
+	  	{
+	  		if (gmarkersType[i] == type)
+	  			if (gmarkers[i].isHidden())
+	  			{
+	  				gmarkers[i].show();
+	  			}
+	  			else {
+	  				gmarkers[i].hide();
+	  			}
+	  	}
+	  }
 
       // This function picks up the click and opens the corresponding info window
       function myclick(i) {
@@ -109,8 +125,9 @@
             var point = new GLatLng(lat,lng);
             var html = markers[i].textContent;
             var label = markers[i].getAttribute("label");
+            var type = markers[i].getAttribute("type");
             // create the marker
-            var marker = createMarker(point,label,html);
+            var marker = createMarker(point,label,html, type);
             map.addOverlay(marker);
           }
           // put the assembled side_bar_html contents into the side_bar div
