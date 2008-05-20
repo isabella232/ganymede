de91b147121a8628fd461291a05447e16db1ca6e
ngervais <ngervais>
2008-05-20 21:04:32 +0000

    *** empty log message ***

--- map.php
+++ map.php
@@ -173,7 +173,7 @@
       // create the map
       var map = new GMap2(document.getElementById("map"));
       map.addControl(new GSmallMapControl());
-      map.setCenter(new GLatLng( 49.345739 ,-75.765338), 1);  //Center on Eclipse Foundation HQ
+      map.setCenter(new GLatLng( 39.345739 ,-75.765338), 1);  //Center on Eclipse Foundation HQ
 
 
       // Read the data from example.xml
