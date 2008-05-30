d07ac04d2cd05c9bec6b80d35955590c54c455c5
ngervais <ngervais>
2008-05-30 16:39:14 +0000

    *** empty log message ***

--- mapList.php
+++ mapList.php
@@ -38,29 +38,36 @@
 	<link type="text/css" href="style.css" rel="stylesheet"/>
 	<body>
 		<div id="midcolumn">
-			<h1><?=$pageTitle;?></h1>
-			<div class="dialog">
- 				<div class="dialogContent">
-  					<div class="t"></div>
-  					<!-- Your content goes here -->
-					<table cellspacing=0 cellpadding=0 class="list" width="750">
-						<tr class="header">	
-							<td>Name</td>
-							<td>Type</td>
-							<td>Location</td>
-							<td>Content</td>
-						</tr>
-						<? while ($rr = mysql_fetch_array($result)) { ?>
-						<tr>
-							<td><?=$rr['name']; ?></td>
-							<td><?=$rr['type']; ?></td>
-							<td><?=$rr['location_city'];?>, <?=$rr['location_country'];?></td>
-							<td><?=$rr['content']; ?></td>
-						</tr>
-						<? } ?>	
-					</table><br/><br/>
+			<div class="mapDiv">
+
+				<div class="dialog">
+	 				<div class="dialogContent">
+	  					<div class="t"></div>
+	  					<!-- Your content goes here -->
+	  					<h1><?=$pageTitle;?></h1><br/>
+						<table cellspacing=0 cellpadding=0 class="list" width="850">
+							<tr class="header">	
+								<td>Name</td>
+								<td>Type</td>
+								<td>Location</td>
+								<td>Content</td>
+							</tr>
+							<? while ($rr = mysql_fetch_array($result)) { ?>
+							<tr>
+								<td><?=$rr['name']; ?></td>
+								<td><?=$rr['type']; ?></td>
+								<td><?=$rr['location_city'];?>, <?=$rr['location_country'];?></td>
+								<td>
+									<? if ($rr['url'] != "") { ?><a href="<?=$rr['url'];?>" target="_blank"><?=$rr['url'];?></a><br/><? } ?>
+									<?=$rr['content']; ?>
+								</td>
+							</tr>
+							<? } ?>	
+						</table><br/><br/>
+					</div>
+	 				<div class="b"><div></div></div>
 				</div>
- 				<div class="b"><div></div></div>
+			</div>
 		</div>
  	</body>
 	<?
