a3f1ac6441de36e98274c06813266f396fab2384
ngervais <ngervais>
2008-06-04 17:29:38 +0000

    *** empty log message ***

--- map.php
+++ map.php
@@ -140,7 +140,7 @@
             var typeText = type;    
             if (type != "Message")
             {
-            	typeText = '<a href"' + url + '" target=_"blank">' + type + '</a>';    
+            	typeText = '<a href="' + url + '" target=_"blank">' + type + '</a>';    
             }
             var html = '<div class="infoWindow"><b>' + author + '</b> - ' + location + '<br/>' + typeText + '<br/><br/>' + markers[i].textContent + '</div>';
             // create the marker
