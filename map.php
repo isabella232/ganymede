b65ebc18a5cf50dd3172c4fad57789ce6024228a
ngervais <ngervais>
2008-06-04 17:19:18 +0000

    *** empty log message ***

--- map.php
+++ map.php
@@ -118,7 +118,7 @@
       // create the map
       var map = new GMap2(document.getElementById("map"));
       map.addControl(new GSmallMapControl());
-      map.setCenter(new GLatLng( geoip_latitude() , geoip_longitude()), 1);  //Center on Eclipse Foundation HQ
+      map.setCenter(new GLatLng(45.345739 ,-75.765338, 1);  //Center on Eclipse Foundation HQ
 	  //map.setMapType(G_HYBRID_MAP);
       // Read the data from example.xml
       var request = GXmlHttp.create();
