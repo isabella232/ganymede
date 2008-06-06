8be4be112d52ce666df2a22324294a44201bb653
lgayowski <lgayowski>
2008-06-06 21:05:34 +0000

    reordered alphabetically

--- postercontest.php
+++ postercontest.php
@@ -40,55 +40,56 @@
 		<p>
 		<table align="top">
 			<tr>
+				<td align="center" width="200"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=103023" targer="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=103023" height="200" border="0"></a><br>
+				<b>Closer than you Might Imagine</b></p></td>
 				<td align="center" width="200"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=102944" targer="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=102944" height="200" border="0"></a><br>
 				<b>Community created... Ganymede!</b></td>
-				<td align="center" width="200"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=102971" targer="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=102971" height="200" border="0"></a><br>
-				<b>Lord Of The Rings</b></td>
-				<td align="center" width="200"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=102974" targer="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=102974" height="200" border="0"></a><br>
-				<b>Uncle Eclipse</b></td>
-				<td align="center" width="200"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=103019" targer="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=103019" height="200" border="0"></a><br>
-				<b>Wall E</b></td>
-			</tr>
-		</table>
-		<table>
-			<tr>
-				<td align="center" width="200"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=103021" targer="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=103021" height="200" border="0"></a><br>
-				<b>Train to Catch</b></td>
-				<td align="center" width="200"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=103022" targer="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=103022" height="200" border="0"></a><br>
-				<b>Train about to Leave</b></td>
-				<td align="center" width="200"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=103024" targer="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=103024" height="200" border="0"></a><br>
-				<b>Ganymede Train</b></td>
+				<td align="center" width="200"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=103205" targer="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=103205" height="200" border="0"></a><br>
+				<b>Enchanted</b></td>
 
 			</tr>
 		</table>
 		<table>
 			<tr>
-				<td align="center" width="200"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=103023" targer="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=103023" height="200" border="0"></a><br>
-				<b>Closer than you Might Imagine</b></p></td>
-				<td align="center" width="200"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=103040" targer="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=103040" height="200" border="0"></a><br>
-				<b>Gan - ee - meed</b></td>				
-				<td align="center" width="200"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=103043" targer="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=103043" height="200" border="0"></a><br>
-				<b>Sunshine</b></td>
+				<td align="center" width="200"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=103024" targer="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=103024" height="200" border="0"></a><br>
+				<b>Ganymede Train</b></td>
+				<td align="center" width="200"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=103138" targer="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=103138" height="200" border="0"></a><br>
+				<b>Gene Simmons Never Had Ganymede</b></td>	
+				<td align="center" width="200"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=102971" targer="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=102971" height="200" border="0"></a><br>
+				<b>Lord Of The Rings</b></td>
 			</tr>
 		</table>
 		<table>
 			<tr>
-				<td align="center" width="200"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=103044" targer="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=103044" height="200" border="0"></a><br>
-				<b>300</b></td>
 				<td align="center" width="200"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=103050" targer="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=103050" height="200" border="0"></a><br>
 				<b>Lost in Translation</b></td>
-				<td align="center" width="200"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=103138" targer="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=103138" height="200" border="0"></a>><br>
-				<b>Gene Simmons Never Had Ganymede</b></td>					
+				<td align="center" width="200"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=103199" targer="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=103199" height="200" border="0"></a><br>
+				<b>Mars Attacks</b></td>				
+				<td align="center" width="200"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=103040" targer="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=103040" height="200" border="0"></a><br>
+				<b>Ratatouille</b></td>				
+				<td align="center" width="200"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=103043" targer="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=103043" height="200" border="0"></a><br>
+				<b>Sunshine</b></td>				
 			</tr>
 		</table>
 		<table>
 			<tr>
-				<td align="center" width="200"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=103199" targer="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=103199" height="200" border="0"></a><br>
-				<b>Mars Attacks</b></td>
-				<td align="center" width="200"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=103205" targer="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=103205" height="200" border="0"></a><br>
-				<b>Enchanted</b></td>
+				<td align="center" width="200"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=103022" targer="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=103022" height="200" border="0"></a><br>
+				<b>Train about to Leave</b></td>
+				<td align="center" width="200"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=103021" targer="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=103021" height="200" border="0"></a><br>
+				<b>Train to Catch</b></td>
 				<td align="center" width="200"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=103391" targer="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=103391" height="200" border="0"></a><br>
 				<b>Transformers</b></td>
+			</tr>
+		</table>
+		<table>
+			<tr>
+				<td align="center" width="200"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=102974" targer="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=102974" height="200" border="0"></a><br>
+				<b>Uncle Eclipse</b></td>
+				<td align="center" width="200"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=103019" targer="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=103019" height="200" border="0"></a><br>
+				<b>Wall E</b></td>
+				<td align="center" width="200"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=103044" targer="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=103044" height="200" border="0"></a><br>
+				<b>300</b></td>
+
 			</tr>
 		</table>
 		</p>
