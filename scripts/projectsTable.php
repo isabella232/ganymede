c1b3b4324645b3d4704d03716a231a0502e8ae9d
ngervais <ngervais>
2008-06-12 15:35:41 +0000

    *** empty log message ***

--- scripts/projectsTable.php
+++ scripts/projectsTable.php
@@ -2,7 +2,7 @@
 
 <? 
 require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/smartconnection.class.php");
-require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/classes/projects/projectInfoList.class.php")
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/classes/projects/projectInfoList.class.php");
 
 function projectTable($pillarType)
 {
