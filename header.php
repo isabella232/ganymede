3e6435a5c35682f97ded1de893bb4e2e3087bb70
ngervais <ngervais>
2008-06-16 17:12:07 +0000

    Ganymede Header

---
+++ header.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" type="text/css" href="/ganymede/header.css" media="screen" />	
+<div id="ganymedeHeader">
+		<div id="headerGraphic">
+		<map name="ganymedeHeaderIMap">
+			 <area shape="rect" coords="163,178,454,260" href="/downloads/packages/">
+		</map>
+		<img src="/ganymede/images/ganymedeHeader.jpg" usemap="#ganymedeHeaderIMap"></div>
+</div>
