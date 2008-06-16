e74f574f25d50fb3673ae89a3d5729c46922e8b2
ngervais <ngervais>
2008-06-16 17:45:45 +0000

    *** empty log message ***

---
+++ headerThin.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" type="text/css" href="/ganymede/headerThin.css" media="screen" />	
+<div id="ganymedeHeader">
+		<div id="headerGraphic">
+		<map name="ganymedeHeaderIMap">
+			 <area shape="rect" coords="163,178,454,260" href="/downloads/packages/">
+		</map>
+		<img src="/ganymede/images/ganymedeHeaderThin.jpg" usemap="#ganymedeHeaderIMap"></div>
+</div>
