76031bd1f0b10b51938b19525f8a0e60d4bb4437
ngervais <ngervais>
2008-06-17 12:40:19 +0000

    *** empty log message ***

--- spotProcess.php
+++ spotProcess.php
@@ -28,7 +28,6 @@
 	$dbc = new DBConnectionRW();
 	$dbh = $dbc->connect();
 	
-	$url = strtolower($url);
 	$query = "INSERT INTO ganymede_spots (id, name, location_city, location_state, location_country, location_lat, location_lng, email) VALUES ('', '$name', '$city', '$state', '$country', $lat, $lng, '$email')";
 	mysql_query($query, $dbh);// or die($query . " - " .mysql_error());
 	$retVal = mysql_insert_id($dbh);
