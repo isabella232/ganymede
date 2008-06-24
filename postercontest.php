350a561d5d8c39634ce76162fed61d412fe45c0b
lgayowski <lgayowski>
2008-06-24 22:08:17 +0000

    reformatted picture spacing

--- postercontest.php
+++ postercontest.php
@@ -70,22 +70,18 @@
 				<b>2nd: Lord Of The Rings (Nick Hofstede)</b></td>
 				<td valign="top" width="163"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=103391" target="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=103391" height="200" width="153" border="0"></a><br>
 				<b>3rd: Transformers (Benjamin Cabe)</b></td>
-			</tr>
-		</table>
-		<table align="top">
-			<tr height="250">
 				<td valign="top" width="161"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=103019" target="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=103019" height="200" width="151" border="0"></a><br>
 				<b>4th: Wall E (Prakash G.R.)</b></td>
 				<td valign="top" width="145"><a href="http://www.eclipse.org/ganymede/images/GanymedeMoon0Poster.jpg" target="blank"><img src="http://www.eclipse.org/ganymede/images/GanymedeMoon0Poster.jpg" height="200" width="250" border="0"></a><br>
 				<b>5th: Some things are closer (Michael Tkacz)</b></td>
-				<td valign="top" width="147"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=104847" target="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=104847"  height="200" width="137" border="0"></a><br>
-				<b>6th (tie): Ice Age (Benjamin Cabe)</b></td>
-				<td valign="top" width="145"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=103040" target="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=103040" height="200" width="135" border="0"></a><br>
-				<b>6th (tie): Ratatouille (Benjamin Cabe)</b></td>					
 			</tr>
 		</table>
 		<table align="top">
 			<tr height="250">
+				<td valign="top" width="147"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=104847" target="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=104847"  height="200" width="137" border="0"></a><br>
+				<b>6th (tie): Ice Age (Benjamin Cabe)</b></td>
+				<td valign="top" width="145"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=103040" target="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=103040" height="200" width="135" border="0"></a><br>
+				<b>6th (tie): Ratatouille (Benjamin Cabe)</b></td>					
 				<td valign="top" width="151"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=102944" target="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=102944" height="200" width="141" border="0"></a><br>
 				<b>8th: Community created... Ganymede! (Benjamin Cabe)</b></td>
 				<td valign="top" width="140"><a href="http://bp1.blogger.com/_i21-98vOfTA/SFIT6BQhm-I/AAAAAAAAAW0/FALIOzx_fMo/s400/children-of-the-code-ganymede.png" target="blank"><img src="http://bp1.blogger.com/_i21-98vOfTA/SFIT6BQhm-I/AAAAAAAAAW0/FALIOzx_fMo/s400/children-of-the-code-ganymede.png" height="200" width="130" border="0"></a><br>
@@ -110,44 +106,40 @@
 				<b>Death Proof (Nick Boldt)</b></td>
 				<td valign="top" width="145"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=103205" target="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=103205" height="200" width="135" border="0"></a><br>
 				<b>Enchanted (Benjamin Cabe)</b></td>
+				<td valign="top" width="180"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=103024" target="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=103024" height="200" width="170" border="0"></a><br>
+				<b>Ganymede Train (Michael Tkacz)</b></td>
 			</tr>
 		</table>
 		<table>
 			<tr height="250">
-				<td valign="top" width="180"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=103024" target="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=103024" height="200" width="170" border="0"></a><br>
-				<b>Ganymede Train (Michael Tkacz)</b></td>
 				<td valign="top" width="278"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=103138" target="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=103138" height="200" width="268" border="0"></a><br>
 				<b>Gene Simmons Never Had Ganymede (Wayne Beaton)</b></td>
 				<td valign="top" width="145"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=104993" target="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=104993"  height="200" width="135" border="0"></a><br>
 				<b>Harry Potter (Nicolas Bihan)</b></td>
 				<td valign="top" width="268"><a href="http://bp0.blogger.com/_i21-98vOfTA/SD98ggANosI/AAAAAAAAAVk/z01lgDfe5gA/s200/p2_Came_From_Beneath_Equinox.png" target="blank"><img src="http://bp0.blogger.com/_i21-98vOfTA/SD98ggANosI/AAAAAAAAAVk/z01lgDfe5gA/s200/p2_Came_From_Beneath_Equinox.png" height="200" width="258" border="0"></a><br>
 				<b>It Came from Beneath the Sea (Nick Boldt)</b></td>
+				<td valign="top" width="277"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=105349" target="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=105349"  height="200" width="267" border="0"></a><br>
+				<b>Kill Bill, Vol. 1 (Kiril Mitov)</b></td>
 			</tr>
 		</table>
 		<table>
 			<tr height="250">	
-				<td valign="top" width="277"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=105349" target="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=105349"  height="200" width="267" border="0"></a><br>
-				<b>Kill Bill, Vol. 1 (Kiril Mitov)</b></td>
 				<td valign="top" width="277"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=105326" target="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=105326"  height="200" width="267" border="0"></a><br>
 				<b>Kill Bill, Vol. 2 (Kivil Mitov)</b></td>
 				<td valign="top" width="138"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=104070" target="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=104070" height="200" width="128" border="0"></a><br>
 				<b>Kung Fu Panda (Nicolas Bihan)</b></td>
-			</tr>
-		</table>
-		<table>
-			<tr height="250">
 				<td valign="top" width="140"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=103050" target="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=103050" height="200" width="130" border="0"></a><br>
 				<b>Lost In Translation (Benjamin Cabe)</b></td>
 				<td valign="top" width="144"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=103199" target="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=103199" height="200" width="134" border="0"></a><br>
 				<b>Mars Attacks (Benjamin Cabe)</b></td>
 				<td valign="top" width="243"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=104052" target="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=104052" height="200" width="233" border="0"></a><br>
 				<b>Prepare yourself for Ganymede (Scott Lewis)</b></td>
-				<td valign="top" width="277"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=105324" target="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=105324"  height="200" width="267" border="0"></a><br>
-				<b>Sin City (Kiril Mitov)</b></td>
 			</tr>
 		</table>
 		<table>
 			<tr height="250">		
+				<td valign="top" width="277"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=105324" target="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=105324"  height="200" width="267" border="0"></a><br>
+				<b>Sin City (Kiril Mitov)</b></td>
 				<td valign="top" width="145"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=103043" target="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=103043" height="200" width="135" border="0"></a><br>
 				<b>Sunshine (Nick Hofstede)</b></td>	
 				<td valign="top" width="148"><a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=104165" target="blank"><img src="https://bugs.eclipse.org/bugs/attachment.cgi?id=104165" height="200" width="138" border="0"></a><br>
