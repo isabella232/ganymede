12430ce3515ec5ea9be9ae879bc46038b17d67fd
ngervais <ngervais>
2008-06-24 19:56:56 +0000

    *** empty log message ***

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <div id="ganymedeHeader">
 		<div id="headerGraphic">
 		<map name="ganymedeHeaderIMap">
-			 <area shape="rect" coords="163,178,454,260" href="/downloads/packages/">
+			 <area shape="rect" coords="460,188,750,250" href="http://friends.eclipse.org/ganymede.html">
 			 <area shape="rect" coords="80,83,447,148" href="/ganymede">
 		</map>
 		<img src="/ganymede/images/ganymedeHeader.jpg" usemap="#ganymedeHeaderIMap"></div>
