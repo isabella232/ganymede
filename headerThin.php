683ad0d7645dbaee98ac7ca78ebe6894d70aa322
ngervais <ngervais>
2009-07-13 15:38:36 +0000

    *** empty log message ***

--- headerThin.php
+++ headerThin.php
@@ -3,7 +3,7 @@
 		<div id="headerGraphic">
 		<map name="ganymedeHeaderIMap">
 			 <area shape="rect" coords="71,61,440, 124" href="/ganymede/">
-			 <area shape="rect" coords="450,60,745,125" href="/downloads/">
+			 <area shape="rect" coords="450,60,745,125" href="/downloads/packages/release/ganymede/sr2">
 		</map>
 		<img src="/ganymede/images/ganymedeHeaderThin.jpg" usemap="#ganymedeHeaderIMap"></div>
 </div>
